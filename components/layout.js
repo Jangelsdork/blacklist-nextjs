425af260e5be675f0239a48b2f2338e627112db6
Jack Mangelsdorf <j.hespe.mangelsdorf@gmail.com>
2023-07-14 18:00:39 +0200

    tracing style ghosts

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Layout({ children }) {
 
     return (
-    <div className={styles.container}>
+    <div>
        <div className='navbar'>
         <Link href="/">Home</Link>
         <Link href="/newSearch">Search</Link>
