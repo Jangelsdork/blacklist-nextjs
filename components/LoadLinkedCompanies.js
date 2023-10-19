4635b9f22b7afb621f29b313076eb7a5b22aa9d1
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2023-10-19 15:17:28 +0200

    Changes in loadlinkedcompany working towards rendering the results from the api call

--- components/LoadLinkedCompanies.js
+++ components/LoadLinkedCompanies.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 //props will need to be the current promoter id 
 const LoadLinkedCompanies = ({ promoterId }) => {
+    const [linkedCompanies, setLinkedCompanies] = useState()
+
     
     useEffect (() => { 
         async function getLinkedCompanies() {
         const response = await fetch("/api/promoter/getLinkedCompany/81");
         const result = await response.json();
-        console.log(result);
+        console.log(result.response.rows);
+        setLinkedCompanies(result.response.rows)
+        renderCompanies()
       } 
     getLinkedCompanies()
-    })
+    },[])
+
+    // at the moment this just console logs the company ID, we want it to render in the dom (and ultimately get the company name)
+    function renderCompanies()  {
+        linkedCompanies.map((line) => (
+        console.log(line.companyId)
+    ))}
     
 
 
     // fetch from PromoterCompany all entries with the reference of promoter id. Can we get organisation name from that? Or do we need to then fetch all the returned ids?
   return (
     //render the names of the organisations 
-    <div>LoadLinkedCompanies </div>
+    <div>nichts</div>
   )
 }
 
